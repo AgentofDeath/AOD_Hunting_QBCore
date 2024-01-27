764a9aadaa93176b724f656f86c917bb85ba0796
earthy <earthyxd@gmail.com>
2024-01-26 20:18:56 -0600

    Delete fxmanifest.lua

--- fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-description 'hunting activity for the QBCore framework https://forum.cfx.re/t/aod-hunting-esx/3492981'
-author 'AgentofDeath'
-version '1.2.0'
-
-client_scripts {'cfg_hunting.lua', 'cl_hunting.lua'}
-
-server_scripts {'cfg_hunting.lua','sv_hunting.lua'}
